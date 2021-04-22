accd1e4bb2099130befa942dd4d42b62953adb6c
hassehulabeck <hansbudandersson@gmail.com>
2021-04-22 11:46:26 +0200

    Added better error-example

--- middleware-error/index.js
+++ middleware-error/index.js
@@ -1,24 +1,28 @@
 const express = require('express')
 const app = express()
 const fs = require('fs')
-
+let simulatedError = false
 
 // Middleware för de två path som vi har.
-app.use('/file', function (req, res, next) {
+app.use('/', (req, res, next) => {
+    // Här kan vi exempelvis kontrollera om ett värde är mindre än ett annat. 
+    // alltså vanlig synkron kod
+    if (simulatedError)     
+        throw new Error('fel vid root-request')
+    else {
+        res.myMessage = 'Hejsan'
+        next()
+    }
+})
+
+app.use('/text', function (req, res, next) {
     fs.readFile('./text.txt',(err, data) => {
         if (err) 
             next(err)
         else
             res.json(data)
-            // Lite märkligt att next inte behövs för "avslut".
+            // Lite märkligt att next inte behövs för "avslut" här.
     })
-})
-
-app.use('/', (req, res, next) => {
-    // Här kan vi exempelvis kontrollera om ett värde är mindre än ett annat. 
-    // alltså vanlig synkron kod
-    throw new Error('fel vid root-request')
-    next()
 })
 
 // Error-hantering
@@ -29,10 +33,10 @@ app.use((err, req, res, next) => {
 
 // Så länge som det inte blir fel körs de här.
 app.get('/', (req, res) => {
-    res.json("Hallå")
+    res.send(res.myMessage)
 })
 
-app.get('/file', (req, res) => {
+app.get('/text', (req, res) => {
     res.json("Hej")
 })
 
