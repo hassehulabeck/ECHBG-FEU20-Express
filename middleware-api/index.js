dc9c1b9eaa7de94da65ea0b743bbc79fc45558d5
hassehulabeck <hansbudandersson@gmail.com>
2021-04-22 13:32:31 +0200

    Added a route

--- middleware-api/index.js
+++ middleware-api/index.js
@@ -1,7 +1,28 @@
 const express = require('express')
+const axios = require('axios')
 const app = express()
-const port = 3000
 
+const port = 3000
+const url = 'https://official-joke-api.appspot.com/random_joke'
+
+app.use( async(req, res, next) => {
+
+    await axios.get(url)
+        .then((res) => {
+            res.joke = res
+        })
+        .error((err) => {
+            console.error(err)
+        })
+        .finally(() => {
+            next()
+        })
+})
+
+
+app.get('/', (req, res) => {
+    res.json(res.joke)
+})
 
 app.listen(port, () => {
     console.log(`Jag lyssnar alltid på ${port}`)
